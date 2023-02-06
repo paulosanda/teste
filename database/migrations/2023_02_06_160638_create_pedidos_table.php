02d94d44fc4dc5115a34b489f60a7de793e137b9
paulosanda <paulosanda@gmail.com>
2023-02-06 13:59:51 -0300

    commit inicial

---
+++ database/migrations/2023_02_06_160638_create_pedidos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->enum('status', ['Aberto', 'Pago', 'Cancelado']);
+            $table->foreringId('cliente_id')->constrained()->onDelete('cascade');
+            $table->integer('valor')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+};
---
+++ database/migrations/2023_02_06_162425_create_pedido_produtos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedido_produtos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pedido_id')->constrained()->onDelete('cascade');
+            $table->string('produto_codigo');
+            $table->string('produto_nome');
+            $table->integer('produto_valor');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedido_produtos');
+    }
+};
